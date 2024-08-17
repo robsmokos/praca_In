cd56a53656e63ec83a102a1308485c4c4fbafbd5
Rob SMOK <robsmok@gmail.com>
2024-08-17 22:47:13 +0200

    skrzyzowanie choinka

--- sumo-rl/experiments/ql_4x4grid_pz.py
+++ sumo-rl/experiments/ql_4x4grid_pz.py
@@ -1,8 +1,7 @@
 import argparse
 import os
 import sys
-
-import pandas as pd
+from datetime import datetime
 
 
 if "SUMO_HOME" in os.environ:
@@ -11,51 +10,79 @@ if "SUMO_HOME" in os.environ:
 else:
     sys.exit("Please declare the environment variable 'SUMO_HOME'")
 
-import traci
 
-import sumo_rl
+from sumo_rl import SumoEnvironment
 from sumo_rl.agents import QLAgent
 from sumo_rl.exploration import EpsilonGreedy
 
 
 if __name__ == "__main__":
-    alpha = 0.1
-    gamma = 0.99
-    decay = 1
-    runs = 1
+    prs = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter, description="""Q-Learning Single-Intersection"""
+    )
+    prs.add_argument(
+        "-route",
+        dest="route",
+        type=str,
+        default="sumo_rl/nets/single-intersection/single-intersection.rou.xml",
+        help="Route definition xml file.\n",
+    )
+    prs.add_argument("-a", dest="alpha", type=float, default=0.1, required=False, help="Alpha learning rate.\n")
+    prs.add_argument("-g", dest="gamma", type=float, default=0.99, required=False, help="Gamma discount rate.\n")
+    prs.add_argument("-e", dest="epsilon", type=float, default=0.05, required=False, help="Epsilon.\n")
+    prs.add_argument("-me", dest="min_epsilon", type=float, default=0.005, required=False, help="Minimum epsilon.\n")
+    prs.add_argument("-d", dest="decay", type=float, default=1.0, required=False, help="Epsilon decay.\n")
+    prs.add_argument("-mingreen", dest="min_green", type=int, default=10, required=False, help="Minimum green time.\n")
+    prs.add_argument("-maxgreen", dest="max_green", type=int, default=50, required=False, help="Maximum green time.\n")
+    prs.add_argument("-gui", action="store_true", default=False, help="Run with visualization on SUMO.\n")
+    prs.add_argument("-fixed", action="store_true", default=False, help="Run with fixed timing traffic signals.\n")
+    prs.add_argument("-ns", dest="ns", type=int, default=42, required=False, help="Fixed green time for NS.\n")
+    prs.add_argument("-we", dest="we", type=int, default=42, required=False, help="Fixed green time for WE.\n")
+    prs.add_argument("-s", dest="seconds", type=int, default=100000, required=False, help="Number of simulation seconds.\n")
+    prs.add_argument("-v", action="store_true", default=False, help="Print experience tuple.\n")
+    prs.add_argument("-runs", dest="runs", type=int, default=1, help="Number of runs.\n")
+    args = prs.parse_args()
+    experiment_time = str(datetime.now()).split(".")[0]
+    out_csv = f"outputs/single-intersection/{experiment_time}_alpha{args.alpha}_gamma{args.gamma}_eps{args.epsilon}_decay{args.decay}"
 
-    env = sumo_rl.env(
-        net_file="sumo_rl/nets/4x4-Lucas/4x4.net.xml",
-        route_file="sumo_rl/nets/4x4-Lucas/4x4c1c2c1c2.rou.xml",
-        use_gui=False,
-        min_green=8,
-        delta_time=5,
-        num_seconds=80000,
+    env = SumoEnvironment(
+        net_file="sumo_rl/nets/single-intersection/single-intersection.net.xml",
+        route_file=args.route,
+        out_csv_name=out_csv,
+        use_gui=args.gui,
+        num_seconds=args.seconds,
+        min_green=args.min_green,
+        max_green=args.max_green,
     )
 
-    for run in range(1, runs + 1):
-        env.reset()
-        initial_states = {ts: env.observe(ts) for ts in env.agents}
+    for run in range(1, args.runs + 1):
+        initial_states = env.reset()
         ql_agents = {
             ts: QLAgent(
-                starting_state=env.unwrapped.env.encode(initial_states[ts], ts),
-                state_space=env.observation_space(ts),
-                action_space=env.action_space(ts),
-                alpha=alpha,
-                gamma=gamma,
-                exploration_strategy=EpsilonGreedy(initial_epsilon=0.05, min_epsilon=0.005, decay=decay),
+                starting_state=env.encode(initial_states[ts], ts),
+                state_space=env.observation_space,
+                action_space=env.action_space,
+                alpha=args.alpha,
+                gamma=args.gamma,
+                exploration_strategy=EpsilonGreedy(
+                    initial_epsilon=args.epsilon, min_epsilon=args.min_epsilon, decay=args.decay
+                ),
             )
-            for ts in env.agents
+            for ts in env.ts_ids
         }
+
+        done = {"__all__": False}
         infos = []
-        for agent in env.agent_iter():
-            s, r, terminated, truncated, info = env.last()
-            done = terminated or truncated
-            if ql_agents[agent].action is not None:
-                ql_agents[agent].learn(next_state=env.unwrapped.env.encode(s, agent), reward=r)
+        if args.fixed:
+            while not done["__all__"]:
+                _, _, done, _ = env.step({})
+        else:
+            while not done["__all__"]:
+                actions = {ts: ql_agents[ts].act() for ts in ql_agents.keys()}
 
-            action = ql_agents[agent].act() if not done else None
-            env.step(action)
+                s, r, done, _ = env.step(action=actions)
 
-        env.unwrapped.env.save_csv("outputs/4x4/pz_ql", run)
+                for agent_id in ql_agents.keys():
+                    ql_agents[agent_id].learn(next_state=env.encode(s[agent_id], agent_id), reward=r[agent_id])
+        env.save_csv(out_csv, run)
         env.close()
